e692bff23384db823499409ff92454aaccfbc9f4
JoCroco <joannysimpore@gmail.com>
2016-11-03 22:23:46 +0100

    Structural Patterns

---
+++ DesignPattern/Bridge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Abstraction ab = new RefinedAbstraction();
+
+            ab.Implementor = new ConcreteImplementorA();
+            ab.Operation();
+
+            ab.Implementor = new ConcreteImplementorB();
+            ab.Operation();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DesignPattern/Composite/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Composite root = new Composite("root");
+            root.Add(new Leaf("Leaf A"));
+            root.Add(new Leaf("Leaf B"));
+
+            Composite comp = new Composite("Composite X");
+            comp.Add(new Leaf("Leaf XA"));
+            comp.Add(new Leaf("Leaf XB"));
+
+            root.Add(comp);
+            root.Add(new Leaf("Leaf C"));
+
+            Leaf leaf = new Leaf("Leaf D");
+            root.Add(leaf);
+            root.Remove(leaf);
+
+            root.Display(1);
+
+            Console.ReadKey();
+        }
+    }
+}
+ 
