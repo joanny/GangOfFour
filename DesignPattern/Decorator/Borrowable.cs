7174f460b62ab2719140aa85e9541724476bd639
JoCroco <joannysimpore@gmail.com>
2016-11-09 19:31:56 +0100

    Decorator Pattern

---
+++ DesignPattern/Decorator/Borrowable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public class Borrowable : Decorator
+    {
+        protected List<string> borrowers = new List<string>();
+
+        public Borrowable(LibraryItem libraryItem)
+            : base(libraryItem)
+        {
+        }
+
+        public void BorrowItem(string name)
+        {
+            if (libraryItem.NumCopies > 0)
+            {
+                borrowers.Add(name);
+                libraryItem.NumCopies--;
+            }
+        }
+
+        public void ReturnItem(string name)
+        {
+            borrowers.Remove(name);
+            libraryItem.NumCopies++;
+        }
+
+        public override void Display()
+        {
+            base.Display();
+
+            foreach (string borrower in borrowers)
+            {
+                Console.WriteLine(" borrower: " + borrower);
+            }
+        }
+    }
+}
